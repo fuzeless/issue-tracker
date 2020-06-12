93fc7d5c1f52247b4d3bfec738b4cd8c371e9e21
Fuzeless <lequangnhat2000@gmail.com>
2020-06-12 18:07:58 +0700

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const app = express();
 const fileServerMiddleware = express.static("public");
 app.use('/', fileServerMiddleware);
 // app.get('/app', fileServerMiddleware);
+
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+`;
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
