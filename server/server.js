9b4dd5a636f192540971e40ffef2d5396f5665ac
Fuzeless <lequangnhat2000@gmail.com>
2020-07-14 17:35:01 +0700

    Implementing API for IssueList

--- server/server.js
+++ server/server.js
@@ -11,6 +11,46 @@ function setAboutMessage(_, { message }) {
     return aboutMessage = message;
 }
 
+//Temp DB for API testing:
+const issuesDB = [
+    {
+        id: 1,
+        status: 'New',
+        owner: 'Fuzeless',
+        created: new Date('2019-05-30'),
+        effort: 5,
+        due: new Date('2019-06-08'),
+        title: 'Missing Title for IssueTracker!!!'
+    },
+    {
+        id: 2,
+        status: 'Closed',
+        owner: 'Ethan',
+        created: new Date('2018-07-19'),
+        effort: 5,
+        due: new Date(""),
+        title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, assumenda!"
+    },
+    {
+        id: 3,
+        status: 'New',
+        owner: 'Fuzeless',
+        created: new Date('2019-05-30'),
+        effort: 5,
+        due: new Date('2019-06-08'),
+        title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+    {
+        id: 4,
+        status: 'New',
+        owner: 'Fuzeless',
+        created: new Date('2019-05-30'),
+        effort: 5,
+        due: new Date('2019-06-08'),
+        title: 'Missing Title for IssueTracker'
+    }
+]
+
 /* The following line is the same as:
     function setAboutMessage(_, { message }) {
         return aboutMessage = message;
@@ -18,12 +58,12 @@ function setAboutMessage(_, { message }) {
 */
 let setName = (_, {name}) => defaultName = name;
 
-//resolvers is json-based
-
+//Resolvers are json-based
 const resolvers = {
     Query: {
         about: () => aboutMessage,
-        name: () => defaultName
+        name: () => defaultName,
+        issueList: () => issuesDB
     },
     Mutation: {
         setAboutMessage,
