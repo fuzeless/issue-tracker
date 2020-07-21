d1ab4748054549d3e74d10b0c6d3aafbecd893e5
Fuzeless <lequangnhat2000@gmail.com>
2020-07-21 14:10:31 +0700

    use environment variable on UI server

--- ui/server.js
+++ ui/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const fileServerMiddleware = express.static('public');
+require('dotenv').config();
 
 app.use(fileServerMiddleware);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.UI_SERVER_PORT || 8000;
 app.listen(PORT, () => {
     console.log(`UI listening on port ${PORT}!`);
 });
