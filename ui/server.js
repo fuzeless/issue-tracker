55d632066f8d9b498596a747fa301f18aa39a8b4
Fuzeless <lequangnhat2000@gmail.com>
2020-07-21 16:42:19 +0700

    implementing proxy for /graphql requests

--- ui/server.js
+++ ui/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const fileServerMiddleware = express.static('public');
+const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 app.use(fileServerMiddleware);
+
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+if (apiProxyTarget) {
+    app.use('/graphql', proxy({ target: apiProxyTarget }));
+}
 
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || "http://localhost:3000/graphql";
 const env = { UI_API_ENDPOINT };
