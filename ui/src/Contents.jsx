5315ef2c93f7dd79256565308f29837039366b9f
Fuzeless <lequangnhat2000@gmail.com>
2020-07-29 10:47:53 +0700

    Add Contents.jsx

---
+++ ui/src/Contents.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+
+import IssueList from './IssueList.jsx';
+import IssueReport from './IssueReport.jsx';
+
+const NotFound = () => <h1>Bitch u directed to the wrong page, go back dumb dumb</h1>;
+
+export default function Contents() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/issue" />
+      <Route path="/issue" component={IssueList} />
+      <Route path="/report" component={IssueReport} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
