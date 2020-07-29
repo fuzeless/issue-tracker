7c8275185dd5094e6eca672513043cd3349b7bdc
Fuzeless <lequangnhat2000@gmail.com>
2020-07-29 11:33:18 +0700

    Center some elements

--- ui/src/IssueRow.jsx
+++ ui/src/IssueRow.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function IssueRow({ issue }) {
   return (
     <tr>
-      <td>{issue.id}</td>
+      <td><center>{issue.id}</center></td>
       <td>{issue.status}</td>
       <td>{issue.owner}</td>
       <td>{issue.created.toDateString()}</td>
-      <td>{issue.effort}</td>
+      <td><center>{issue.effort}</center></td>
       <td>{issue.due ? issue.due.toDateString() : ' '}</td>
       <td>{issue.title}</td>
-      <td><a href={`/#/edit/${issue.id}`}>Edit</a></td>
+      <td><a href={`/#/edit/${issue.id}`}><center>Edit</center></a></td>
     </tr>
   );
 }
