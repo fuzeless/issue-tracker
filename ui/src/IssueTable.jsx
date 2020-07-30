7b7bc4afa155436f1613dfb90b9d4d90d51b5d3f
Fuzeless <lequangnhat2000@gmail.com>
2020-07-30 15:27:24 +0700

    Merge IssueRow and IssueTable

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
-import IssueRow from './IssueRow.jsx';
+import { withRouter, Link, NavLink } from 'react-router-dom';
+
+const IssueRow = withRouter(({ issue, location: { search } }) => {
+  const selectLocation = { pathname: `/issues/${issue.id}`, search };
+  return (
+    <tr>
+      <td><center>{issue.id}</center></td>
+      <td>{issue.status}</td>
+      <td>{issue.owner}</td>
+      <td>{issue.created.toDateString()}</td>
+      <td><center>{issue.effort}</center></td>
+      <td>{issue.due ? issue.due.toDateString() : ' '}</td>
+      <td>{issue.title}</td>
+      <td>
+        <Link to={`edit/${issue.id}`}>Edit</Link>
+        {' | '}
+        <NavLink to={selectLocation}>Select</NavLink>
+      </td>
+    </tr>
+  );
+});
 
 export default function IssueTable({ issues }) {
   const issueRows = issues.map((issue) => <IssueRow key={issue.id} issue={issue} />);
