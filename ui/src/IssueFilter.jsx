ebbf584109c240c68d3d2f5befe0e7f4f2fe0b46
Fuzeless <lequangnhat2000@gmail.com>
2020-07-30 11:18:53 +0700

    Use simple dropdown filter, Link removed

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+
+  onChangeStatus(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+    history.push({
+      pathname: '/issues',
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=New' }}>New Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>Assigned Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=Closed' }}>Closed Issues</Link>
+        Status:
+        { }
+        <select onChange={this.onChangeStatus}>
+          <option value="">(All)</option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Closed">Closed</option>
+        </select>
       </div>
     );
   }
 }
+// Default Class doesn't have any Routes => no location, history component
+
+export default withRouter(IssueFilter);
