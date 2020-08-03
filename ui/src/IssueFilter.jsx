cc796cc7027305a0184f0b6683ece3135732ecbf
Fuzeless <lequangnhat2000@gmail.com>
2020-08-03 11:22:26 +0700

    Improvements to IssueFilter

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -4,14 +4,34 @@ import URLSearchParams from 'url-search-params';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
-  constructor() {
+  constructor({ location: { search } }) {
     super();
+    const params = new URLSearchParams(search);
+    this.state = {
+      status: params.get('status') || '',
+      changed: false,
+    };
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.resetFilter = this.resetFilter.bind(this);
+    this.showOriginalFilter = this.showOriginalFilter.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (search !== prevSearch) this.showOriginalFilter();
   }
 
   onChangeStatus(e) {
-    const status = e.target.value;
-    console.log(this.props);
+    this.setState({
+      status: e.target.value,
+      changed: true,
+    });
+  }
+
+  applyFilter() {
+    const { status } = this.state;
     const { history } = this.props;
     history.push({
       pathname: '/issues',
@@ -19,19 +39,50 @@ class IssueFilter extends React.Component {
     });
   }
 
-  render() {
+  showOriginalFilter() {
     const { location: { search } } = this.props;
     const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || '',
+      changed: false,
+    });
+  }
+
+  resetFilter() {
+    this.setState({
+      status: '',
+    });
+    const { history } = this.props;
+    history.push({
+      pathname: '/issues',
+      search: '',
+    });
+  }
+
+  render() {
+    // const { location: { search } } = this.props;
+    // const params = new URLSearchParams(search);
+    const { status, changed } = this.state;
     return (
       <div>
         Status:
-        {}
-        <select value={params.get('status')} onChange={this.onChangeStatus}>
+        {' '}
+        <select value={status} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
           <option value="Closed">Closed</option>
         </select>
+        {' | '}
+        <button type="button" onClick={this.applyFilter}>Apply</button>
+        {' | '}
+        <button
+          type="button"
+          onClick={this.showOriginalFilter}
+          disabled={!changed}
+        >
+          Reset
+        </button>
       </div>
     );
   }
