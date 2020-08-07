409d52d977f846b7de0c10dcf296e7f67d260ee3
Fuzeless <lequangnhat2000@gmail.com>
2020-08-07 18:16:05 +0700

    Implement Bootstrap Navbar to Page's Nav

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  Nav, Navbar, NavItem, NavDropdown,
+  MenuItem, Tooltip, OverlayTrigger,
+} from 'react-bootstrap';
+import { FaPlus, FaChevronDown, FaEllipsisV } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import Contents from './Contents.jsx';
 
-function Navbar() {
+function Navigation() {
+  const createIssueTooltip = <Tooltip id="tooltip-create">Create Issue</Tooltip>
   return (
     <div>
-      <NavLink exact to="/">Home</NavLink>
-      {' | '}
-      <NavLink to="/issues">Issue List</NavLink>
-      {' | '}
-      <NavLink to="/report">Report</NavLink>
+      <Navbar expand="lg" bg="dark" variant="dark" fixed="top">
+        <Navbar.Brand>
+          Issue Tracker
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <LinkContainer exact to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/issues">
+            <Nav.Link>Issue List</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/report">
+            <Nav.Link>Report</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        <Nav>
+          {/* Create Issue icon */}
+          <Nav.Link>
+            <OverlayTrigger
+              placement="left"
+              overlay={createIssueTooltip}
+            >
+              <FaPlus />
+            </OverlayTrigger>
+          </Nav.Link>
+          <NavDropdown
+            id="user-dropdown"
+            title={<FaChevronDown />}
+            noCaret
+          >
+            <NavDropdown.Item>About</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
@@ -17,7 +52,7 @@ function Navbar() {
 export default function Page() {
   return (
     <div>
-      <Navbar />
+      <Navigation />
       <Contents />
     </div>
   );
