d44b5f40035f36ad8d19c0516ed84e0cf764575a
Fuzeless <lequangnhat2000@gmail.com>
2020-08-17 14:39:38 +0700

    Re-use runtime environment variable injection method for JS

--- ui/src/graphql_fetch.js
+++ ui/src/graphql_fetch.js
@@ -10,7 +10,7 @@ function jsonDateReviver(key, value) {
 // Fetch GraphQL Data
 export default async function graphQLFetch(query, variables = {}) {
   try {
-    const response = await fetch(__UI_API_ENDPOINT__, {
+    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ query, variables }),
