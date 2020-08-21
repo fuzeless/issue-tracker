d1fc3c146069c2deb7983183c56294a7080ba276
Fuzeless <lequangnhat2000@gmail.com>
2020-08-21 16:33:55 +0700

    New IssueReport page

--- ui/src/IssueReport.jsx
+++ ui/src/IssueReport.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Button, Card, Collapse } from 'react-bootstrap';
 
-export default function IssueReport() {
-  return (
-    <div>
-      <h1>Placeholder for Issue Report</h1>
-    </div>
-  );
+import IssueFilter from './IssueFilter.jsx';
+
+export default class IssueReport extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isFilterOpened: false,
+    }
+  }
+
+  render() {
+    const { isFilterOpened } = this.state;
+    return (
+      <>
+        <Card bg="dark" text="white">
+          <Card.Header>
+            <Button
+              style={{ backgroundColor: 'transparent', border: 'transparent' }}
+              block
+              onClick={() => this.setState({ isFilterOpened: !isFilterOpened })}
+            >
+              <h4>Filter</h4>
+            </Button>
+          </Card.Header>
+          <Collapse in={isFilterOpened}>
+            <div>
+              <Card.Body>
+                <IssueFilter urlBase="/report" />
+              </Card.Body>
+            </div>
+          </Collapse>
+        </Card>
+      </>
+    );
+  }
 }
